1dc50d61a53ba2a3ac412a21cb8ad39b17662b9d
Luca Saiu <saiu@lipn.univ-paris13.fr>
2009-04-09 15:37:43 +0200

    Added a new wrapper for GNU Gettext, written by yours truly

---
+++ gettext/gettext-example.ml
@@ -0,0 +1,31 @@
+(* This file is part of Marionnet, a virtual network laboratory
+   Copyright (C) 2009  Luca Saiu
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>. *)
+
+
+(* This is just a trivial example showing how to use our gettext wrapper: *)
+
+open Gettext;;
+
+(* This uses the translations of GNU Hello, (http://www.gnu.org/software/hello)
+   which are assumed to be found in /usr/share/locale. Of course you can change
+   this: *)
+initialize_gettext "hello" "/usr/share/locale";;
+
+(* The strings below are really used in GNU Hello. Of course you can't replace
+   them with any arbitrary message, unless you have translations for them: *)
+Printf.printf "%s\n" (gettext "hello, world\n");;
+Printf.printf "%s\n" (gettext "Try `%s --help' for more information.\n");;
+Printf.printf "%s\n" (gettext "Report bugs to <%s>.\n");;
---
+++ gettext/gettext.ml
@@ -0,0 +1,31 @@
+(* This file is part of Marionnet, a virtual network laboratory
+   Copyright (C) 2009  Luca Saiu
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>. *)
+
+
+(* Make the primitives implemented in C visible to the OCaml world: *)
+external non_thread_safe_gettext_primitive : string -> string = "gettext_primitive";;
+external initialize_gettext : string -> string -> unit = "initialize_gettext_primitive";;
+
+(* gettext() isn't thread-safe, so we should sequentialize concurrent calls
+   with a mutex: *)
+let the_mutex = Mutex.create ();;
+
+(* Wrap the main primitive within a thread-safe function: *)
+let gettext string_in_english =
+  Mutex.lock the_mutex;
+  let result = non_thread_safe_gettext_primitive string_in_english in
+  Mutex.unlock the_mutex;
+  result;;
