8c953a9463ad2cac5488180a9cad61dcf13085f4
Jean-Vincent Loddo <loddo@lipn.univ-paris13.fr>
2009-06-25 15:53:05 +0200

    Enhanced support for chips, wires and systems tracing (for debugging).

--- chip/chip.ml
+++ chip/chip.ml
@@ -34,7 +34,70 @@ type performed = Performed of bool
 let max_number_of_iterations = 1024
 let current_system = ref None
 
+type tracing_policy = 
+ | Top_tracing_policy (** If the system is traced, all components are traced. *)
+ | Bot_tracing_policy (** Each component decides individually. *)
+
+let tracing_policy = Top_tracing_policy
+
 class
+
+ tracing_methods ~(name:string) ~(tracing:bool) =
+  object (self)
+
+  val mutable tracing = tracing
+  method tracing = tracing
+  method set_tracing x = tracing <- x
+
+  val mutable tracing_level = 0
+  method tracing_level = tracing_level
+  method tracing_tab prompt =
+   let n = self#tracing_level in
+   (String.make n ' ')^(String.make (n+1) prompt)^" "
+
+  method tracing_lparen msg =
+   if tracing = false then () else
+    begin
+     self#tracing_message_with_prompt '+' msg;
+     tracing_level <- tracing_level + 1 ;
+    end
+
+  method tracing_rparen msg =
+   if tracing = false then () else
+    begin
+     tracing_level <- tracing_level - 1 ;
+     self#tracing_message_with_prompt '-' msg;
+    end
+
+  method tracing_message_with_prompt prompt msg =
+   if tracing = false then () else
+    begin
+     Printf.eprintf "%s%s: %s\n" (self#tracing_tab prompt) name msg;
+     flush stderr
+    end
+
+  method tracing_message msg =
+   if tracing = false then () else self#tracing_message_with_prompt '*' msg
+
+  end (* object *)
+
+and
+
+ virtual tracing_methods_for_components ~(name:string) system =
+  object (self)
+   inherit tracing_methods ~name ~tracing:system#tracing as super
+   method tracing = match tracing_policy with
+    | Top_tracing_policy -> tracing || system#tracing
+    | Bot_tracing_policy -> tracing
+   method tracing_level = system#tracing_level + tracing_level
+   method private format_message  msg = (Printf.sprintf "%s (%d): %s" name self#id msg)
+   method tracing_lparen  msg = system#tracing_lparen  (self#format_message msg)
+   method tracing_rparen  msg = system#tracing_rparen  (self#format_message msg)
+   method tracing_message msg = system#tracing_message (self#format_message msg)
+   method virtual id : int
+  end
+
+and
 
  (** Common features for chips, wires and systems. *)
  common ~(name:string) =
@@ -42,20 +105,15 @@ class
    val id = fresh_id ()
    method id = id
    method name = name
-   method trace ?msg tracing_method_name =
-    begin
-    (match msg with 
-     | None     -> (Printf.eprintf "In %s#%s (id=%d)\n"  self#name tracing_method_name self#id)
-     | Some msg -> (Printf.eprintf "%s#%s (id=%d): %s\n" self#name tracing_method_name self#id msg)
-     ); (flush stderr)
-    end
  end
 
 and
 
  (** Common features for chips and wires. This class just performs the system subscription. *)
  component ~(name:string) system =
-  object (self) inherit common ~name
+  object (self)
+  inherit common ~name
+  inherit tracing_methods_for_components ~name system
   val mutable system = system
   method system = system
   initializer
@@ -106,6 +164,19 @@ and
  virtual reactive ?(name = fresh_chip_name "reactive") (system:system) =
   object (self) inherit chip ~name system
   method virtual stabilize : performed
+  method traced_stabilize =
+   if self#tracing = false then self#stabilize else
+    begin
+      self#tracing_lparen "stabilization...";
+      let result = self#stabilize in
+      let msg = match result with
+       | Performed false -> "chip already stabilized."
+       | Performed true  -> "stabilized."
+      in
+      self#tracing_rparen msg;
+      result
+    end
+
   initializer
    system#add_reactive (self :> reactive)
  end
@@ -133,8 +204,14 @@ and
 and
 
  (** A system is a container of chips and wires. *)
- system ?(name = fresh_system_name "system") ?(max_number_of_iterations = max_number_of_iterations) () =
-  object (self) inherit common ~name
+ system
+  ?(name = fresh_system_name "system")
+  ?(max_number_of_iterations = max_number_of_iterations)
+  ?(tracing = true) (* on stderr *)
+  ()
+  = object (self)
+  inherit common ~name
+  inherit tracing_methods ~name ~tracing
 
   (* Wires perform their operations (get,set) preventing from effects of other threads.
      In a single-thread application this support is unnecessary. *)
@@ -152,7 +229,8 @@ and
       result
     with e -> begin
       self#unlock;
-      (Printf.eprintf "exception %s raised in critical section. Unlocking and re-raising." (Printexc.to_string e));
+      (Printf.eprintf "System %s: exception %s raised in critical section. Unlocking and re-raising."
+          name (Printexc.to_string e));
       (flush stderr);
       raise e;
       end
@@ -176,6 +254,18 @@ and
   method relay_list : reactive list = List.filter (fun c->c#kind=`Relay) reactive_list
   method sink_list  : reactive list = List.filter (fun c->c#kind=`Sink)  reactive_list
 
+  (* For debugging: *)
+  method show_component_list () =
+   begin
+    Printf.eprintf "System: %s\n" name;
+    List.iter (fun c -> Printf.eprintf "\t* %s (%d)\n" c#name c#id) component_list;
+    Printf.eprintf "\t Relays: ";
+    List.iter (fun c -> Printf.eprintf "%s (%d) " c#name c#id) self#relay_list;
+    Printf.eprintf "\n\t Sinks: ";
+    List.iter (fun c -> Printf.eprintf "%s (%d) " c#name c#id) self#sink_list;
+    Printf.eprintf "\n---\n"; flush stderr
+   end
+
   (** The system stabilize itself calling the stabilization of its reactive chips.
       Relays are firstly stabilized with a stabilization loop and finally sinks are stabilized
       in a single step. The result indicated if the stabilization has really changed the state
@@ -186,13 +276,19 @@ and
     if (acc >= max_number_of_iterations)
      then (failwith (Printf.sprintf "system#stabilize: stabilization failed after %d iterations" max_number_of_iterations))
      else begin
-      let performed_list = List.map (fun (x:reactive) -> x#stabilize) relay_list in
+      let performed_list = List.map (fun (x:reactive) -> x#traced_stabilize) relay_list in
       if List.exists ((=)(Performed true)) performed_list then loop (acc+1) else acc
      end
   in
+  self#tracing_lparen "system stabilization...";
+  self#tracing_lparen "relays' stabilization...";
   let loops = loop 0 in
-  let sink_results = List.map (fun (x:reactive) -> x#stabilize) self#sink_list in
+  self#tracing_rparen "relays' stabilized.";
+  self#tracing_lparen "sinks' stabilization...";
+  let sink_results = List.map (fun (x:reactive) -> x#traced_stabilize) self#sink_list in
   let result = (loops > 0) || (List.exists ((=)(Performed true)) sink_results) in
+  self#tracing_rparen "sinks' stabilized.";
+  self#tracing_rparen "system stabilized.";
   (Performed result)
 
  initializer
@@ -214,26 +310,37 @@ and
 
   (** This would be a final method. *)
   method get : 'b =
+   self#tracing_message "reading wire value (get)";
    let action () = self#get_alone in
    self#system#with_mutex action
 
   (** This would be a final method. *)
   method set (x:'a) : unit =
+   self#tracing_lparen "setting wire...";
    let action () =
      begin
      (self#set_alone x);
      ignore (self#system#stabilize);
+     self#tracing_rparen "wire set.";
      end
    in self#system#with_mutex action
 
  end
 
 (** References are simple examples of wires. *)
-class ['a] wref ?(name = fresh_wire_name "wref") (system:system) (value:'a) =
-  object (self) inherit ['a,'a] wire ~name system
+class ['a] wref
+ ?(name = fresh_wire_name "wref") (system:system) (value:'a) =
+ object (self) inherit ['a,'a] wire ~name system
   val mutable content = value
   method get_alone   = content
   method set_alone v = (content <- v)
+ end
+
+class ['a] wire_of_accessors
+ ?(name = fresh_wire_name "wire_of_accessors") (system:system) get_alone set_alone =
+ object (self) inherit ['a,'a] wire ~name system
+  method get_alone = get_alone ()
+  method set_alone = set_alone
  end
 
 type ('b,'a) in_port  = (('b,'a) wire * 'a) option
@@ -282,6 +389,20 @@ let get_or_initialize_current_system () = match !current_system with
  | None   -> new system ()
  | Some s -> s
 
-(** Simplified constructor. *)
+(** Simplified constructors. *)
 let wref ?(name = fresh_wire_name "wref") ?(system=(get_or_initialize_current_system ())) x =
  new wref ~name system x
+
+let wire_of_accessors
+ ?(name = fresh_wire_name "wire_of_accessors")
+ ?(system=(get_or_initialize_current_system ())) ~(get:unit -> 'a) ~(set:'a->unit) () =
+ ((new wire_of_accessors ~name system get set) :> ('a,'a) wire)
+
+(** Useful method for building "toggling chips" (chips that toggle their boolean output when a change occurs in their inputs). *)
+(*class toggle_method () =
+ object
+  val mutable __hidden_state = false
+  method toggle =
+   let () = (__hidden_state <- not __hidden_state) in
+   __hidden_state
+ end*)
