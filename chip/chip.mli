59f75fb43c45affe075b46d2299e1a3181665737
Jean-Vincent Loddo <loddo@lipn.univ-paris13.fr>
2009-11-23 17:29:19 +0100

    Added chip.mli and an experimental wrapper for GTK buttons (converting events into wire settings)

---
+++ chip/chip.mli
@@ -0,0 +1,437 @@
+type performed = Performed of bool
+val max_number_of_iterations : int
+
+type tracing_policy = Top_tracing_policy | Bot_tracing_policy
+val tracing_enable : unit -> bool
+val tracing_policy : tracing_policy
+
+class type tracing_methods =
+  object
+    method set        : bool -> unit
+    method is_enabled : bool
+    method level      : int
+    method lparen     : string -> unit
+    method message    : string -> unit
+    method rparen     : string -> unit
+    method tab        : char -> string
+  end
+
+class ['a] container :
+  list_name:string ->
+  owner:string ->
+  with_mutex: ((unit->unit) -> unit) ->
+  object
+    constraint 'a = < tracing : tracing_methods;  add_destroy_callback : (unit->unit) -> unit; .. >
+    method content : 'a list
+    method remove  : 'a -> unit
+    method add     : 'a -> unit
+  end
+
+class mutex_methods : ?mutex:Recursive_mutex.t -> user:string -> unit ->
+  object
+    method mutex      : Recursive_mutex.t
+    method lock       : unit
+    method unlock     : unit
+    method with_mutex : (unit -> 'a) -> 'a
+  end
+
+class virtual destroy_methods : unit ->
+  object
+    method add_destroy_callback : (unit -> unit) -> unit
+    method destroy : unit -> unit
+    method private virtual tracing : tracing_methods
+  end
+
+class common :
+  ?id:int -> name:string -> ?parent:common -> unit ->
+  object
+    method id : int
+    method name : string
+    method parent : common option
+    method set_parent : common option -> unit
+    method parent_list : common list
+    method as_common : common
+    method dot_reference : string
+  end
+
+class virtual component :
+  name:string ->  parent:common -> system ->
+  object
+    method id : int
+    method name : string
+    method parent : common option
+    method set_parent : common option -> unit
+    method parent_list : common list
+    method as_common : common
+    method dot_reference : string
+    method system : system
+    method destroy : unit -> unit
+    method add_destroy_callback : (unit -> unit) -> unit
+    method tracing : tracing_methods
+    method virtual to_dot : string
+  end
+
+and virtual chip : ?name:string -> system ->
+  object
+    method id : int
+    method name : string
+    method parent : common option
+    method set_parent : common option -> unit
+    method parent_list : common list
+    method as_common : common
+    method dot_reference : string
+    method system : system
+    method destroy : unit -> unit
+    method add_destroy_callback : (unit -> unit) -> unit
+    method tracing : tracing_methods
+
+    val virtual in_port_names : string list
+    val virtual out_port_names : string list
+    method in_port_names : string list
+    method out_port_names : string list
+    method port_names : string list
+    method kind : [ `Relay | `Sink | `Source ]
+    method is_reactive : bool
+    method virtual input_wire_connections  : (string * (common option)) list
+    method virtual output_wire_connections : (string * (common option)) list
+    method to_dot : string
+  end
+
+and virtual source : ?name:string -> system ->
+  object
+    method id : int
+    method name : string
+    method parent : common option
+    method set_parent : common option -> unit
+    method parent_list : common list
+    method as_common : common
+    method dot_reference : string
+    method system : system
+    method destroy : unit -> unit
+    method add_destroy_callback : (unit -> unit) -> unit
+    method tracing : tracing_methods
+
+    val virtual in_port_names : string list
+    val virtual out_port_names : string list
+    method in_port_names : string list
+    method out_port_names : string list
+    method port_names : string list
+    method kind : [ `Relay | `Sink | `Source ]
+    method is_reactive : bool
+    method virtual input_wire_connections  : (string * (common option)) list
+    method virtual output_wire_connections : (string * (common option)) list
+    method to_dot : string
+  end
+
+and virtual reactive : ?name:string -> system ->
+  object
+    method id : int
+    method name : string
+    method parent : common option
+    method set_parent : common option -> unit
+    method parent_list : common list
+    method as_common : common
+    method dot_reference : string
+    method system : system
+    method destroy : unit -> unit
+    method add_destroy_callback : (unit -> unit) -> unit
+    method tracing : tracing_methods
+
+    val virtual in_port_names : string list
+    val virtual out_port_names : string list
+    method in_port_names : string list
+    method out_port_names : string list
+    method port_names : string list
+    method kind : [ `Relay | `Sink | `Source ]
+    method is_reactive : bool
+    method virtual input_wire_connections  : (string * (common option)) list
+    method virtual output_wire_connections : (string * (common option)) list
+    method to_dot : string
+
+    method virtual stabilize : performed
+    method traced_stabilize : performed
+  end
+
+and virtual relay : ?name:string -> system ->
+  object
+    method id : int
+    method name : string
+    method parent : common option
+    method set_parent : common option -> unit
+    method parent_list : common list
+    method as_common : common
+    method dot_reference : string
+    method system : system
+    method destroy : unit -> unit
+    method add_destroy_callback : (unit -> unit) -> unit
+    method tracing : tracing_methods
+
+    val virtual in_port_names : string list
+    val virtual out_port_names : string list
+    method in_port_names : string list
+    method out_port_names : string list
+    method port_names : string list
+    method kind : [ `Relay | `Sink | `Source ]
+    method is_reactive : bool
+    method virtual input_wire_connections  : (string * (common option)) list
+    method virtual output_wire_connections : (string * (common option)) list
+    method to_dot : string
+
+    method virtual stabilize : performed
+    method traced_stabilize : performed
+  end
+
+and virtual sink : ?name:string -> system ->
+  object
+    method id : int
+    method name : string
+    method parent : common option
+    method set_parent : common option -> unit
+    method parent_list : common list
+    method as_common : common
+    method dot_reference : string
+    method system : system
+    method destroy : unit -> unit
+    method add_destroy_callback : (unit -> unit) -> unit
+    method tracing : tracing_methods
+
+    val virtual in_port_names : string list
+    val virtual out_port_names : string list
+    method in_port_names : string list
+    method out_port_names : string list
+    method port_names : string list
+    method kind : [ `Relay | `Sink | `Source ]
+    method is_reactive : bool
+    method virtual input_wire_connections  : (string * (common option)) list
+    method virtual output_wire_connections : (string * (common option)) list
+    method to_dot : string
+
+    method virtual stabilize : performed
+    method traced_stabilize : performed
+  end
+
+and system :
+  ?father:system ->
+  ?name:string ->
+  ?max_number_of_iterations:int ->
+  ?tracing:(unit->bool) ->
+  ?set_default_system:bool ->
+  unit ->
+  object
+    method id : int
+    method name : string
+    method parent : common option
+    method set_parent : common option -> unit
+    method parent_list : common list
+    method as_common : common
+    method dot_reference : string
+    method mutex_methods : mutex_methods
+    method tracing : tracing_methods
+
+    method reactive_list  : reactive container
+    method component_list : component container
+    method relay_list     : relay list
+    method sink_list      : sink list
+    method show_component_list : unit -> unit
+    method to_dot : string
+
+    method stabilize : performed
+  end
+
+and virtual ['a, 'b] wire : ?name:string -> ?parent:common -> system ->
+  object
+    method id : int
+    method name : string
+    method parent : common option
+    method set_parent : common option -> unit
+    method parent_list : common list
+    method as_common : common
+    method dot_reference : string
+    method system : system
+    method destroy : unit -> unit
+    method add_destroy_callback : (unit -> unit) -> unit
+    method tracing : tracing_methods
+    method to_dot : string
+
+    method set : 'a -> unit
+    method get : 'b
+    method virtual set_alone : 'a -> unit
+    method virtual get_alone : 'b
+  end
+
+class ['a] wref : ?name:string -> ?parent:common -> system -> 'a ->
+  object
+    method id : int
+    method name : string
+    method parent : common option
+    method set_parent : common option -> unit
+    method parent_list : common list
+    method as_common : common
+    method dot_reference : string
+    method system : system
+    method destroy : unit -> unit
+    method add_destroy_callback : (unit -> unit) -> unit
+    method tracing : tracing_methods
+    method to_dot : string
+
+    method set : 'a -> unit
+    method get : 'a
+    method set_alone : 'a -> unit
+    method get_alone : 'a
+  end
+
+class wcounter : ?name:string -> ?parent:common -> system ->
+  object
+    method id : int
+    method name : string
+    method parent : common option
+    method set_parent : common option -> unit
+    method parent_list : common list
+    method as_common : common
+    method dot_reference : string
+    method system : system
+    method destroy : unit -> unit
+    method add_destroy_callback : (unit -> unit) -> unit
+    method tracing : tracing_methods
+    method to_dot : string
+
+    method set : unit -> unit
+    method get : int
+    method set_alone : unit -> unit
+    method get_alone : int
+  end
+
+class ['a, 'b] cable :
+  ?name:string   ->
+  ?tracing:(unit->bool)  ->
+  ?parent:common ->
+  system ->
+  object
+    method id : int
+    method name : string
+    method parent : common option
+    method set_parent : common option -> unit
+    method parent_list : common list
+    method as_common : common
+    method dot_reference : string
+    method system : system
+    method destroy : unit -> unit
+    method add_destroy_callback : (unit -> unit) -> unit
+    method tracing : tracing_methods
+    method to_dot : string
+
+    method wire_list : ('a,'b) wire container
+
+    method set : int * 'a -> unit
+    method get : (int * 'b) list
+    method set_alone : int * 'a -> unit
+    method get_alone : (int * 'b) list
+end
+
+class type ['a] writeonly_wire =
+  object
+    method id : int
+    method name : string
+    method parent : common option
+    method set_parent : common option -> unit
+    method parent_list : common list
+    method as_common : common
+    method dot_reference : string
+    method system : system
+    method destroy : unit -> unit
+    method add_destroy_callback : (unit -> unit) -> unit
+    method tracing : tracing_methods
+    method to_dot : string
+    method set : 'a -> unit
+    method set_alone : 'a -> unit
+  end
+
+class type ['b] readonly_wire =
+  object
+    method id : int
+    method name : string
+    method parent : common option
+    method set_parent : common option -> unit
+    method parent_list : common list
+    method as_common : common
+    method dot_reference : string
+    method system : system
+    method destroy : unit -> unit
+    method add_destroy_callback : (unit -> unit) -> unit
+    method tracing : tracing_methods
+    method to_dot : string
+    method get : 'b
+    method get_alone : 'b
+  end
+
+class ['a] option_obj : ?name:string -> ?owner:string -> 'a option ->
+  object
+    method get : 'a option
+    method set : 'a option -> unit
+    method extract : 'a
+    method map  : 'b. ('a -> 'b) -> 'b option
+    method bind : 'b. ('a -> 'b option) -> 'b option
+  end
+
+class ['a] in_port_connection : equality:('a -> 'a -> bool) -> ('b, 'a) wire ->
+  object
+    method read_and_compare : bool -> 'a * bool
+    method readonly_wire : 'a readonly_wire
+  end
+
+class ['a] in_port :
+  ?sensitiveness:bool ->
+  ?equality:('a -> 'a -> bool) ->
+  name:string ->
+  ?wire:('b, 'a) wire ->
+  unit ->
+  object
+    method name : string
+    method connect : 'c. ('c, 'a) wire -> unit
+    method connection : ('a in_port_connection) option_obj
+    method disconnect : unit
+    method sensitiveness : bool
+    method equality : 'a -> 'a -> bool
+    method read_and_compare : 'a * bool
+    method set_equality : ('a -> 'a -> bool) -> unit
+    method set_sensitiveness : bool -> unit
+  end
+
+class ['a] out_port_connection : ('a, 'b) wire ->
+ object
+  method writeonly_wire : 'a writeonly_wire
+ end
+
+class ['a] out_port :
+  name:string ->
+  ?wire:('a, 'b) wire ->
+  unit ->
+  object
+    method name : string
+    method connect : 'c. ('a, 'c) wire -> unit
+    method connection : ('a out_port_connection) option_obj
+    method disconnect : unit
+  end
+
+val teach_ocamldep : unit
+val initialize : unit -> unit
+val stabilize : unit -> performed
+val get_current_system : unit -> system
+val get_or_initialize_current_system : unit -> system
+
+val wref     : ?name:string -> ?parent:common -> ?system:system -> 'a -> 'a wref
+val wcounter : ?name:string -> ?parent:common -> ?system:system -> unit -> wcounter
+val cable : ?name:string -> ?parent:common -> ?system:system -> unit -> ('a, 'a) cable
+val wref_in_cable : ?name:string -> cable:('a,'a) cable -> 'a -> 'a wref
+
+val current_system : system option ref
+val wire_of_accessors : ?name:string -> ?parent:common -> ?system:system -> get:(unit -> 'b) -> set:('a -> unit) -> unit -> ('a,'b) wire
+
+val fresh_chip_name   : string -> string
+val fresh_source_name : string -> string
+val fresh_relay_name  : string -> string
+val fresh_sink_name   : string -> string
+val fresh_wire_name   : string -> string
+val fresh_system_name : string -> string
+
+val dot_record_of_port_names :string list -> string
---
+++ chip/wGButton.ml
@@ -0,0 +1,89 @@
+
+class button_wire_tuple ~owner ~system =
+ let clicked  = Chip.wcounter ~name:(owner^"#clicked") ~system () in
+ let enter    = Chip.wcounter ~name:(owner^"#enter")   ~system () in
+ let leave    = Chip.wcounter ~name:(owner^"#leave")   ~system () in
+ let pressed  = Chip.wcounter ~name:(owner^"#pressed") ~system () in
+ let released = Chip.wcounter ~name:(owner^"#clicked") ~system () in
+object
+ method clicked  = clicked
+ method enter    = enter
+ method leave    = leave
+ method pressed  = pressed
+ method released = released
+
+ method destroy () =
+  clicked#destroy ();
+  enter#destroy ();
+  leave#destroy ();
+  pressed#destroy ();
+  released#destroy ()
+end
+
+type button_signal = Clicked | Enter | Leave | Pressed | Released
+
+class button
+ ?name
+ ?parent
+ (system:Chip.system)
+ obj =
+ let name = match name with None -> Chip.fresh_wire_name "button" | Some x -> x in
+ let wire = new button_wire_tuple ~owner:name ~system in
+object (self)
+ inherit GButton.button (obj : Gtk.button Gtk.obj) as self_as_gbutton
+ method wire = wire
+
+ inherit [ button_signal, (int * int * int * int * int)] Chip.wire ~name ?parent system as self_as_wire
+
+ method destroy () =
+  wire#destroy ();
+  self_as_wire#destroy ();
+  self_as_gbutton#destroy ()
+
+ method set_alone = function
+ | Clicked  -> wire#clicked#set ()
+ | Enter    -> wire#enter#set ()
+ | Leave    -> wire#leave#set ()
+ | Pressed  -> wire#pressed#set ()
+ | Released -> wire#released#set ()
+
+ method get_alone =
+  (wire#clicked#get,
+   wire#enter#get,
+   wire#leave#get,
+   wire#pressed#get,
+   wire#released#get)
+
+  method to_dot =
+   Printf.sprintf "%d [shape=record, label=\"%s (%d) | {|{%s}|}\"];"
+     self#id
+     self#name
+     self#id
+     (Chip.dot_record_of_port_names
+         [wire#clicked#name;
+	  wire#enter#name;
+	  wire#leave#name;
+	  wire#pressed#name;
+	  wire#released#name ])
+
+initializer
+ ignore (self#connect#clicked  ~callback:(wire#clicked#set));
+ ignore (self#connect#enter    ~callback:(wire#enter#set));
+ ignore (self#connect#leave    ~callback:(wire#leave#set));
+ ignore (self#connect#pressed  ~callback:(wire#pressed#set));
+ ignore (self#connect#released ~callback:(wire#released#set));
+ let p = (Some self#as_common) in
+  wire#clicked#set_parent  p;
+  wire#enter#set_parent    p;
+  wire#leave#set_parent    p;
+  wire#pressed#set_parent  p;
+  wire#released#set_parent p;
+  ()
+end
+
+let pack_return create p ?packing ?show () =
+ GObj.pack_return (create p) ~packing ~show
+
+let button ?name ?parent (system:Chip.system) ?label =
+  GtkButton.Button.make_params [] ?label ~cont:(
+  pack_return (fun p -> new button ?name ?parent system (GtkButton.Button.create p)))
