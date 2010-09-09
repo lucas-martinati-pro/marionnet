fd88e58779d9f1ba4b6e266096557a4090f20cdb
Jean-Vincent Loddo <loddo@lipn.univ-paris13.fr>
2010-09-09 17:16:49 +0200

    Some parts of initialization.ml rewritten using Log. The behaviour has not changed (widely tested).

--- initialization.ml
+++ initialization.ml
@@ -1,5 +1,6 @@
 (* This file is part of Marionnet, a virtual network laboratory
    Copyright (C) 2007, 2008  Luca Saiu
+   Copyright (C) 2010  Jean-Vincent Loddo
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -14,10 +15,6 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>. *)
 
-
-open Unix;;
-open Sys;;
-open Gettext;;
 
 Printf.printf "=======================================================
  Welcome to %s
@@ -44,22 +41,6 @@ Printf.printf "=======================================================
   Meta.build_date
   (StringExtra.fmt ~tab:8 ~width:40 Meta.uname)
 ;;  
-(*(** First of all print the version number. This is useful for problem reports: *)
-Printf.printf "========================================================\n" ;;
-Printf.printf " Welcome to %s version %s revision %s\n"
-  Meta.name
-  Meta.version Meta.revision;;
-Printf.printf " %s\n" Meta.uname;;
-Printf.printf " Source date: %s\n" Meta.source_date;;
-Printf.printf " Built in date: %s\n" Meta.build_date;;
-Printf.printf " Using ocamlbricks revno %s (%s)\n"
-Printf.printf " Please get a launchpad account and either:\n";;
-Printf.printf " - report bugs at https://bugs.launchpad.net/marionnet\n" ;;
-Printf.printf " or do *all* the following:\n";;
-Printf.printf " - add yourself to the marionnet-dev team\n";;
-Printf.printf " - add yourself to the marionnet-dev mailing list\n";;
-Printf.printf " - write to marionnet-dev@lists.launchpad.net\n";;
-Printf.printf "========================================================\n\n" ;;*)
 
 (* Seed the random number generator: *)
 Random.self_init ();;
@@ -85,6 +66,7 @@ let configuration =
                 "MARIONNET_KERNELS_PATH";
                 "MARIONNET_VDE_PREFIX";
                 "MARIONNET_ROUTER_FILESYSTEM";
+                "TEST";
               ]
     ();;
 
@@ -92,92 +74,63 @@ let configuration =
 let cwd_at_startup_time =
   Unix.getcwd ();;
 
-(* We can not use Log.printf here because Initialization is a top file 
-   use by Log (and  others), do that creates circular dependencies *)
-(*Printf.printf "Setting up directory path names for Marionnet...\n";; *)
+(** Return the value of the given configuration variable, if it's defined as
+    a non-empty string; otherwise return the default.
+    The third argument is a continuation that may be useful, for instance,
+    to add a slash if it is a pathname. *)
+let configuration_variable_or ?k ?(default="") variable_name =
+  let fallback e x = Log.printf ~force:true "Warning: %s not found.\n" x in
+  let result =
+    match Option.of_fallible_application ~fallback configuration#string variable_name
+    with
+    | None
+    | Some "" -> default
+    | Some x  -> x
+   in
+   (* Launch the continuation on the result: *)
+   match k with None -> result | Some f -> (f result)
+ ;;
 
-(** Return the value of the given configuration variable, if it's defined as 
-    a non-empty string; otherwise return the second argument, third argument
-    gives the type of variable in order to add a slash if it is a pathname *)
-let configuration_variable_or_ variable_name default_value var_type =
-  let fallback_value = 
-    if var_type = "type_path" then
-      default_value ^ "/" 
-    else
-      default_value in
-  try
-    let variable_value =
-      configuration#string variable_name in
-    if var_type = "type_path" then
-      (if variable_value = "" then
-        default_value ^ "/"
-      else
-        variable_value ^ "/")
-    else
-      (if variable_value = "" then
-        default_value
-      else
-        variable_value)
-  with Not_found ->
-    fallback_value;;
+(* Used as continuation (~k) calling configuration_variable_or: *)
+let append_slash x = x ^ "/" ;;
 
-(** Wrapper of configuration_variable_or_ function, display values 
-    of a given variable during its initialization *) 
-let configuration_variable_or variable_name default_value var_type =
-  let result = configuration_variable_or_ variable_name default_value var_type in
-(*  Printf.printf "\n %s (%s) = %s\n" variable_name default_value result;*)
-  result;;
-
-(* Here we initialze some variables with user or default value *)
 let marionnet_home =
-  configuration_variable_or
-    "MARIONNET_PREFIX" 
-    (Meta.prefix ^ "/share/" ^ Meta.name) 
-    "type_path";;
+  let default = (Meta.prefix ^ "/share/" ^ Meta.name) in
+  configuration_variable_or ~k:append_slash ~default "MARIONNET_PREFIX" ;;
+
 let marionnet_home_filesystems =
-  configuration_variable_or
-    "MARIONNET_FILESYSTEMS_PATH"
-    (marionnet_home^"/filesystems/") 
-    "type_path" ;;
+  let default = (marionnet_home^"/filesystems/") in
+  configuration_variable_or ~k:append_slash ~default "MARIONNET_FILESYSTEMS_PATH" ;;
+
 let marionnet_home_kernels =
-  configuration_variable_or
-    "MARIONNET_KERNELS_PATH"
-    (marionnet_home^"/kernels/") 
-    "type_path";;
+  let default = (marionnet_home^"/kernels/") in
+  configuration_variable_or ~k:append_slash ~default "MARIONNET_KERNELS_PATH" ;;
+
 (* The prefix to prepend to VDE executables; this allows us to install
     patched versions in an easy way, before our changes are integrated
     into VDE's mainline... *)
-let vde_prefix =
-  configuration_variable_or
-    "MARIONNET_VDE_PREFIX"
-    "" 
-    "type_other";;
+let vde_prefix = configuration_variable_or "MARIONNET_VDE_PREFIX";;
+
 (* What is terminal that Marionnet must use to lanch a virtual host *)
 let marionnet_terminal =
-  configuration_variable_or
-    "MARIONNET_TERMINAL"
-    "xterm,-T,-e"
-    "type_other";;
-let marionnet_home_images =
-  marionnet_home^"/images/";;
-let marionnet_home_bin =
-  marionnet_home^"/bin/";;
+  let default = "xterm,-T,-e" in
+  configuration_variable_or ~default "MARIONNET_TERMINAL" ;;
+
+let marionnet_home_images = marionnet_home^"/images/";;
+let marionnet_home_bin = marionnet_home^"/bin/";;
 
 (* Default for the factory-set configuration address for routers.
    The result is a couple (ip,nm) where ip is the 4-tuple IPv4 and nm is the CIDR netmask. *)
 let router_port0_default_ipv4_config  =
- let str = configuration_variable_or
-    "MARIONNET_ROUTER_PORT0_DEFAULT_IPV4_CONFIG"
-    "192.168.1.254/24"
-    "type_other"
- in Scanf.sscanf str "%i.%i.%i.%i/%i" (fun b1 b2 b3 b4 nm -> ((b1, b2, b3, b4), nm))
-;;
-
-(* Useful for setting spin buttons. => ip library ? *)
-let router_port0_default_ipv4_config_float_converted =
- let f = float_of_int in
- let ((ip1,ip2,ip3,ip4),nm) = router_port0_default_ipv4_config in
- (((f ip1),(f ip2),(f ip3),(f ip4)),(f nm))
+ let variable_name = "MARIONNET_ROUTER_PORT0_DEFAULT_IPV4_CONFIG" in
+ let default = "192.168.1.254/24" in
+ let value = configuration_variable_or ~default variable_name in
+ let parse arg = Scanf.sscanf value "%i.%i.%i.%i/%i" (fun b1 b2 b3 b4 nm -> ((b1, b2, b3, b4), nm)) in
+ try parse value 
+ with _ -> begin
+   Log.printf ~force:true "Warning: ill-formed value for %s\n" variable_name;
+   parse default
+   end
 ;;
 
 (* Enter the right directory: *)
@@ -185,6 +138,3 @@ try
   Unix.chdir marionnet_home;
 with _ ->
   failwith ("Could not enter the directory (" ^ marionnet_home ^ ")");;
-
-(* inizialize gettext with marionnet as textdomain and the default path where to find .mo files *)
-(* initialize_gettext "marionnet" "/usr/share/locale";; *)
