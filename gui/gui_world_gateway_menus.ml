daa288c94845ca96c0fe5d9ee67e941148eee446
Jean-Vincent Loddo <loddo@lipn.univ-paris13.fr>
2010-09-21 19:14:28 +0200

    Gui world gateway related stuff rewritten using a type-safe structure instead of string environments. The code is now more robust, quite less cryptic and more concise (2 files instead of 3 for managing all world-gateways related menus (4 if we consider gui.xml)). The idea is to re-use the same code structure for routers and, may be, for the other components.

--- gui/gui_world_gateway_menus.ml
+++ gui/gui_world_gateway_menus.ml
@@ -23,7 +23,7 @@ type env  = string Environment.string_env
 let mkenv = Environment.make_string_env
 let env_to_string t = t#to_string (fun s->s)
 
-module Make_menus (State : sig val st:State.globalState end) = struct
+module Make (State : sig val st:State.globalState end) = struct
 
   open State
 
@@ -33,66 +33,111 @@ module Make_menus (State : sig val st:State.globalState end) = struct
   end
 
   module Add = struct
-    type t = env
-    let to_string = env_to_string
+    type t = [ `name of string                  ] *
+	     [ `label of string                 ] *
+	     [ `network_config of Ipv4.config   ] *
+	     [ `dhcp_enabled   of bool          ] *
+	     [ `user_port_no   of int           ] *
+	     [ `oldname        of string        ]
+
+    let to_string t = "<obj>" (* TODO *)
     let key      = Some GdkKeysyms._G
 
-    let dialog   = let module M = Gui_world_gateway_dialog.Make (State) in
-                   M.dialog ~title:(s_ "Add world gateway") ~update:None
+    let ok_callback ((((`name name),_,_,_,_,_) as g):t) =
+      if not (StrExtra.wellFormedName name)
+        then None   (* refused *)
+        else Some g (* accepted *)
 
-    let reaction r =
-      let defects = Defects_interface.get_defects_interface () in
-      let name  = r#get "name" in
-      let label = r#get "label" in
-      let network_address = r#get "network_address" in
-      let dhcp_enabled = bool_of_string (r#get "dhcp_enabled") in
-      let user_port_no = int_of_string (r#get "user_port_no") in
+    let help_callback () = () (* TODO *)
+
+    let dialog () =
+      let name = st#network#suggestedName "G" in
+      let s_ = Gettext.s_ in
+      Gui_world_gateway_dialog.make
+        ~title:(s_ "Add world gateway") ~s_ ~name ~ok_callback ~help_callback ()
+
+    let network_address_of_config config =
+      let ((i1,i2,i3,_),_) = config in
+      Printf.sprintf "%i.%i.%i.%i" i1 i2 i3 0
+   
+    let reaction : t -> unit =
+     function
+      (`name name,
+       `label label,
+       `network_config network_config,
+       `dhcp_enabled dhcp_enabled,
+       `user_port_no user_port_no,
+       `oldname _)
+      ->
       let g =
         new Mariokit.Netmodel.world_gateway
           ~network:st#network
           ~name
           ~label
-          ~network_address
+          ~network_address:(network_address_of_config network_config)
           ~dhcp_enabled
           ~user_port_no
           ()
       in
       (* The "world" port is hidden for defects: *)
-      (** VERIFICARE il tipo "router"!! **)
+      let defects = Defects_interface.get_defects_interface () in
       defects#add_device name "router" user_port_no;
       st#network_change st#network#add_world_gateway g;
   end
 
   module Properties = struct
-    type t = env
-    let to_string = env_to_string
+    type t = Add.t
+    let to_string = Add.to_string
 
     let dynlist =
      fun () -> List.filter
                   (fun x -> (st#network#get_world_gateway_by_name x)#can_startup)
                   (st#network#get_world_gateway_names)
 
-    let dialog name =
-     let m = (st#network#get_world_gateway_by_name name) in
-     let title = (s_ "Modify world gateway") in
-     let module M = Gui_world_gateway_dialog.Make (State) in M.dialog ~title:(title^" "^name) ~update:(Some m)
+    let dialog name () =
+     let g = (st#network#get_world_gateway_by_name name) in
+     let title = (s_ "Modify world gateway")^" "^name in
+     let label = g#get_label in
+     (* With the current version of slirpvde i4 is always 1 and cidr is 24 *)
+     let network_config =
+       let fixed_cidr = 24 in
+       ((Ipv4.ipv4_of_string g#get_network_address), fixed_cidr)
+     in
+     let dhcp_enabled = g#get_dhcp_enabled in
+     let user_port_no = g#get_eth_number in
+     Gui_world_gateway_dialog.make
+       ~title ~s_ ~name ~label ~network_config ~dhcp_enabled ~user_port_no
+       ~ok_callback:Add.ok_callback ~help_callback:Add.help_callback ()
 
-    let reaction r =
+
+    let reaction : t -> unit =
+     function
+      (`name name,
+       `label label,
+       `network_config network_config,
+       `dhcp_enabled dhcp_enabled,
+       `user_port_no user_port_no,
+       `oldname oldname)
+      ->
       let defects = Defects_interface.get_defects_interface () in
-      let (name, oldname) = (r#get "name"), (r#get "oldname") in
       let d = st#network#get_world_gateway_by_name oldname in
-      d#destroy; (* VERIFICARE *)
-      st#network#ledgrid_manager#destroy_device_ledgrid ~id:(d#id) ();
-      st#network#change_node_name oldname name  ;
-      d#set_label (r#get "label");
-      d#set_network_address (r#get "network_address");
-      d#set_dhcp_enabled (bool_of_string (r#get "dhcp_enabled"));
-      let user_port_no = int_of_string (r#get "user_port_no") in
+      (match name = oldname with
+      | true -> ()
+      | false ->
+          begin
+            d#destroy; (* VERIFICARE *)
+            st#network#ledgrid_manager#destroy_device_ledgrid ~id:(d#id) ();
+            st#network#change_node_name oldname name  ;
+            Filesystem_history.rename_device oldname name;
+            defects#rename_device oldname name;
+            st#network#make_device_ledgrid (d :> Mariokit.Netmodel.device);
+          end
+      | _ -> ()
+      );
+      d#set_label label;
+      d#set_network_address (Add.network_address_of_config network_config);
+      d#set_dhcp_enabled dhcp_enabled;
       d#set_eth_number ~prefix:"port" user_port_no;
-      st#refresh_sketch () ;
-      st#network#make_device_ledgrid (d :> Mariokit.Netmodel.device);
-      Filesystem_history.rename_device oldname name;
-      defects#rename_device oldname name;
       defects#update_port_no name user_port_no;
       st#update_cable_sensitivity ()
 
