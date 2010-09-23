6151836c70411ef4c56da4972ba4e032afd6a30b
Jean-Vincent Loddo <loddo@lipn.univ-paris13.fr>
2010-09-23 10:09:16 +0200

    Removed file gui/gui/gui_world_gateway_dialog.ml. We have now a single file (gui/gui_world_gateway.ml) for the gui related code of the "world gateway" component, and the implementation is totally independent from glade. The new code structure uses  the "where" syntax extension defined in ocamlbricks revno 196.

--- gui/gui_world_gateway.ml
+++ gui/gui_world_gateway.ml
@@ -22,8 +22,28 @@ open Gettext;;
 type env  = string Environment.string_env
 let mkenv = Environment.make_string_env
 let env_to_string t = t#to_string (fun s->s)
+;;
 
-module Make (State : sig val st:State.globalState end) = struct
+(* The module containing the add/update dialog is defined later,
+   using the syntax extension "where" *)
+#load "where_p4.cmo"
+;;
+
+(* The type of data exchanged with the dialog: *)
+module Data = struct
+type t = {
+  name           : string;
+  label          : string;
+  network_config : Ipv4.config;
+  dhcp_enabled   : bool;
+  user_port_no   : int;
+  oldname        : string;
+  }
+
+let to_string t = "<obj>" (* TODO? *)
+end (* Data *)
+
+module Make_menus (State : sig val st:State.globalState end) = struct
 
   open State
 
@@ -33,28 +53,22 @@ module Make (State : sig val st:State.globalState end) = struct
   end
 
   module Add = struct
-    type t = [ `name of string                  ] *
-	     [ `label of string                 ] *
-	     [ `network_config of Ipv4.config   ] *
-	     [ `dhcp_enabled   of bool          ] *
-	     [ `user_port_no   of int           ] *
-	     [ `oldname        of string        ]
+    include Data
 
-    let to_string t = "<obj>" (* TODO *)
-    let key      = Some GdkKeysyms._G
+    let key = Some GdkKeysyms._G
 
-    let ok_callback ((((`name name),_,_,_,_,_) as g):t) =
-      if not (StrExtra.wellFormedName name)
+    let ok_callback t =
+      if not (StrExtra.wellFormedName t.name)
         then None   (* refused *)
-        else Some g (* accepted *)
+        else Some t (* accepted *)
 
     let help_callback () = () (* TODO *)
 
     let dialog () =
       let name = st#network#suggestedName "G" in
       let s_ = Gettext.s_ in
-      Gui_world_gateway_dialog.make
-        ~title:(s_ "Add world gateway") ~s_ ~name ~ok_callback ~help_callback ()
+      Dialog_add_or_update.make
+        ~title:(s_ "Add world gateway") ~s_ ~name ~ok_callback (*~help_callback TODO*) ()
 
     let network_address_of_config config =
       let ((i1,i2,i3,_),_) = config in
@@ -62,12 +76,12 @@ module Make (State : sig val st:State.globalState end) = struct
    
     let reaction : t -> unit =
      function
-      (`name name,
-       `label label,
-       `network_config network_config,
-       `dhcp_enabled dhcp_enabled,
-       `user_port_no user_port_no,
-       `oldname _)
+      { name = name;
+        label = label;
+        network_config = network_config;
+        dhcp_enabled = dhcp_enabled;
+        user_port_no = user_port_no;
+        oldname = _ }
       ->
       let g =
         new Mariokit.Netmodel.world_gateway
@@ -86,8 +100,7 @@ module Make (State : sig val st:State.globalState end) = struct
   end
 
   module Properties = struct
-    type t = Add.t
-    let to_string = Add.to_string
+    include Data
 
     let dynlist =
      fun () -> List.filter
@@ -105,19 +118,19 @@ module Make (State : sig val st:State.globalState end) = struct
      in
      let dhcp_enabled = g#get_dhcp_enabled in
      let user_port_no = g#get_eth_number in
-     Gui_world_gateway_dialog.make
+     Dialog_add_or_update.make
        ~title ~s_ ~name ~label ~network_config ~dhcp_enabled ~user_port_no
        ~ok_callback:Add.ok_callback ~help_callback:Add.help_callback ()
 
 
     let reaction : t -> unit =
      function
-      (`name name,
-       `label label,
-       `network_config network_config,
-       `dhcp_enabled dhcp_enabled,
-       `user_port_no user_port_no,
-       `oldname oldname)
+      { name = name;
+        label = label;
+        network_config = network_config;
+        dhcp_enabled = dhcp_enabled;
+        user_port_no = user_port_no;
+        oldname = oldname }
       ->
       let defects = Defects_interface.get_defects_interface () in
       let d = st#network#get_world_gateway_by_name oldname in
@@ -139,7 +152,8 @@ module Make (State : sig val st:State.globalState end) = struct
       d#set_dhcp_enabled dhcp_enabled;
       d#set_eth_number ~prefix:"port" user_port_no;
       defects#update_port_no name user_port_no;
-      st#update_cable_sensitivity ()
+      st#update_cable_sensitivity ();
+      st#refresh_sketch ();
 
   end
 
@@ -221,3 +235,110 @@ module Make (State : sig val st:State.globalState end) = struct
   Gui_toolbar_COMPONENTS_layouts.Layout_for_network_node (State) (Toolbar_entry) (Add) (Properties) (Remove) (Startup) (Stop) (Suspend) (Resume)
 
 end
+
+(*-----*)
+  WHERE
+(*-----*)
+
+module Dialog_add_or_update = struct
+
+(* This function may be useful for testing the widget creation without
+   recompiling the whole project. *)
+let make
+ ?(title="Add a world gateway")
+ ?(s_=fun x->x) (* Gettext support. Not useful when testing. *)
+ ?(name="")
+ ?label
+ ?(network_config:Ipv4.config option)
+ ?(dhcp_enabled=true)
+ ?(user_port_no=4)
+ ?help_callback
+ ?(ok_callback=(fun data -> Some data))
+ ?(dialog_image_file=Initialization.Path.images^"ico.world_gateway.dialog.png")
+ () :'result option =
+  let oldname = name in
+  let ((b1,b2,b3,b4),b5) = match network_config with
+   | Some x -> x
+   | None   -> ((10,0,2,1),24)
+  in
+  let icon =
+    let icon_file = Initialization.Path.images^"marionnet-launcher.png" in
+    GdkPixbuf.from_file icon_file
+  in
+  let w = GWindow.dialog ~icon ~title ~modal:true ~position:`CENTER () in
+  let tooltips = Gui_bricks.make_tooltips_for_container w in
+
+  let (name,label) =
+    let hbox = GPack.hbox ~homogeneous:true ~border_width:20 ~spacing:10 ~packing:w#vbox#add () in
+    let image = GMisc.image ~file:dialog_image_file ~xalign:0.5 ~packing:hbox#add () in
+    tooltips image#coerce (s_ "Gateway");
+    let vbox = GPack.vbox ~spacing:10 ~packing:hbox#add () in
+    let name  = Gui_bricks.entry_with_label ~packing:vbox#add ~entry_text:name  (s_ "Name") in
+    let label = Gui_bricks.entry_with_label ~packing:vbox#add ?entry_text:label (s_ "Label") in
+    tooltips name#coerce (s_ "Gateway name. This name must be unique in the virtual network. Suggested: G1, G2, ...");
+    tooltips label#coerce (s_ "Label to be written in the network sketch, next to the element icon." );
+    (name,label)
+  in
+
+  ignore (GMisc.separator `HORIZONTAL ~packing:w#vbox#add ());
+
+  let ((s1,s2,s3,s4,s5), dhcp_enabled, user_port_no) =
+    let vbox = GPack.vbox ~homogeneous:false ~border_width:20 ~spacing:10 ~packing:w#vbox#add () in
+    let form =
+      Gui_bricks.make_form_with_labels
+        ~packing:vbox#add
+        [(s_ "IPv4 address"); (s_ "DHCP service"); (s_ "Integrated switch ports")]
+    in
+    let network_config =
+      Gui_bricks.spin_ipv4_address_with_cidr_netmask
+        ~packing:form#add
+        b1 b2 b3 b4 b5
+    in
+    let dhcp_enabled =
+      GButton.check_button
+        ~active:dhcp_enabled
+        ~packing:form#add ()
+    in
+    let user_port_no =
+      Gui_bricks.spin_byte ~lower:2 ~upper:16 ~step_incr:2 ~packing:form#add user_port_no
+    in
+    tooltips form#coerce (s_ "IPv4 configuration and required services" );
+    tooltips dhcp_enabled#coerce (s_ "The gateway should provide a DHCP service?" );
+    (network_config, dhcp_enabled, user_port_no)
+  in
+  tooltips s1#coerce (s_ "First octet of the IPv4 address" );
+  tooltips s2#coerce (s_ "Second octet of the IPv4 address" );
+  tooltips s3#coerce (s_ "Third octet of the IPv4 address" );
+  tooltips s4#coerce (s_ "Fourth octet of the IPv4 address" );
+  tooltips s5#coerce (s_ "Netmask (CIDR notation)" );
+  tooltips user_port_no#coerce (s_ "The number of ports of the integrated switch" );
+  s4#misc#set_sensitive false;
+  s5#misc#set_sensitive false;
+
+  let get_widget_data () :'result =
+    let name = name#text in
+    let label = label#text in
+    let network_config =
+      let s1 = int_of_float s1#value in
+      let s2 = int_of_float s2#value in
+      let s3 = int_of_float s3#value in
+      let s4 = int_of_float s4#value in
+      let s5 = int_of_float s5#value in
+      ((s1,s2,s3,s4),s5)
+    in
+    let dhcp_enabled = dhcp_enabled#active in
+    let user_port_no = int_of_float user_port_no#value in
+      { Data.name = name;
+        Data.label = label;
+        Data.network_config = network_config;
+        Data.dhcp_enabled = dhcp_enabled;
+        Data.user_port_no = user_port_no;
+        Data.oldname = oldname; }
+  in
+  (* The result of make is the result of the dialog loop (of type 'result option): *)
+  Gui_bricks.dialog_loop w ~ok_callback ?help_callback ~get_widget_data ()
+
+end
+
+(** Just for testing: *)
+let test = Dialog_add_or_update.make
