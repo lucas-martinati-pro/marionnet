3e4751d704807d89c2b57d1153231e595967ce4b
Jean-Vincent Loddo <loddo@lipn.univ-paris13.fr>
2010-11-14 20:17:19 +0100

    Added the IUTV and LIPN logos in the splash window and updated the about dialog.

--- splash.ml
+++ splash.ml
@@ -19,18 +19,25 @@ open Gettext;;
 
 (* span foreground="red" *)
 
-let splashscreen_text =
-"<i>Marionnet " ^ Version.version ^ "</i>, " ^ Version.build_time ^ ".
+let text_title =
+  Printf.sprintf
+    "<small><b>%s</b></small>"
+    (s_ "Marionnet, a virtual network laboratory")
+;;
 
-Copyright (C) 2007, 2008, 2009 Jean-Vincent Loddo
-Copyright (C) 2007, 2008, 2009 Luca Saiu
+let text_subtitle =
+"<small><i>Version " ^ Version.version ^ "</i> revno " ^ Meta.revision ^ " - " ^ Meta.source_date ^ "</small>"
+;;
 
-Sponsored by Université Paris 13
+let text =
+"<small>Copyright (C) 2007, 2008, 2009, 2010 Jean-Vincent Loddo
+Copyright (C) 2007, 2008, 2009, 2010 Luca Saiu
+Copyright (C) 2007, 2008, 2009, 2010 Université Paris 13
 
-<span style=\"italic\">Marionnet comes with <b>absolutely no warranty</b>.
+<i>Marionnet comes with <b>absolutely no warranty</b>.
 This is free software, covered by the GNU GPL.
 You are welcome to redistribute it under certain
-conditions; see the file `COPYING' for details.</span>";;
+conditions; see the file `COPYING' for details.</i></small>";;
 
 let handle_click window _ =
   Log.printf "handle_click: the splash screen was closed\n";
@@ -40,7 +47,7 @@ let handle_click window _ =
 
 let splash_image =
   GDraw.pixmap_from_xpm
-    ~file:(Initialization.Path.images^"splash.xpm")
+    ~file:(Initialization.Path.images^"splash.300x348.xpm")
     ();;
 
 let splash =
@@ -53,11 +60,40 @@ let splash =
     ~wm_name:"Marionnet splash screen"
     ~icon:Icon.icon_pixbuf
     ();;
+
 splash#set_title (s_ "Welcome to Marionnet");;
 let event_box = GBin.event_box ~packing:splash#add () in
-let box = GPack.vbox ~border_width:2 ~packing:event_box#add () in
-let _ = GMisc.pixmap splash_image ~packing:(box#pack ~padding:3) () in
-let _ = GMisc.label ~markup:splashscreen_text ~packing:box#add ~line_wrap:false () in
+let box = GPack.vbox ~spacing:5 ~border_width:2 ~packing:event_box#add () in
+let _image = GMisc.pixmap splash_image ~packing:(box#pack ~padding:3) () in
+let _title =
+  let align = GBin.alignment ~xalign:1. ~packing:box#add () in
+  let table = GPack.table ~rows:2 ~columns:1 ~row_spacings:0 ~homogeneous:false ~packing:(align#add) () in
+  let attach = table#attach ~expand:`X ~fill:`BOTH ~left:0 in
+  let _ = GMisc.label ~markup:text_title ~packing:(attach ~top:0) ~xalign:0.5 ~line_wrap:false () in
+  let _ = GMisc.label ~markup:text_subtitle ~packing:(attach ~top:1) ~xalign:0.5 ~line_wrap:false () in
+  ()
+in
+let _ = GMisc.label ~markup:text ~packing:box#add ~line_wrap:false () in
+let table =
+  GPack.table ~rows:1 ~columns:3 ~col_spacings:20
+    ~homogeneous:true
+    ~packing:box#add ()
+in
+let _logo_paris13 =
+ GMisc.image
+   ~file:(Initialization.Path.images^"logo.paris13.96x96.png")
+   ~xalign:0.5 ~packing:(table#attach ~left:0 ~top:0) ()
+in
+let _logo_iutv =
+ GMisc.image
+   ~file:(Initialization.Path.images^"logo.iutv.96x96.png")
+   ~xalign:0.5 ~packing:(table#attach ~left:1 ~top:0) ()
+in
+let _logo_lipn =
+ GMisc.image
+   ~file:(Initialization.Path.images^"logo.lipn.96x96.png")
+   ~xalign:0.5 ~packing:(table#attach ~left:2 ~top:0) ()
+in
 let _ = event_box#event#connect#button_press ~callback:(handle_click splash) in
 let _ = splash#event#connect#key_press ~callback:(fun ev -> handle_click splash ()) in ()
 ;;
