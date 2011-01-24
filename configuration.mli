b6c3c049524e83dbe8cade97633cd370d801bf05
Jean-Vincent Loddo <loddo@lipn.univ-paris13.fr>
2011-01-24 16:42:51 +0100

    Added the module Configuration, included by Initialization and Marionnet-daemon.

---
+++ configuration.mli
@@ -0,0 +1,10 @@
+val configuration : Configuration_files.configuration
+(*
+  object
+    method bool   : string -> bool
+    method float  : string -> float
+    method int    : string -> int
+    method list   : string -> string list
+    method string : string -> string
+  end
+*)
