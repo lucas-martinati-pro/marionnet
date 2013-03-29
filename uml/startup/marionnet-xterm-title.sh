cfa56238f17c589c52187156e144414e40b36b04
Jean-Vincent Loddo <loddo@lipn.univ-paris13.fr>
2013-03-29 15:58:11 +0100

    Other UML's init scripts imported (cfg2html,..) from virtual machines.

--- uml/startup/marionnet-xterm-title.sh
+++ uml/startup/marionnet-xterm-title.sh
@@ -1,9 +1,16 @@
-# --L.
+#!/bin/bash
 
+# Author: Luca Saiu 
+# Date: 2007
+# Modified (minor changes) by Jean-Vincent Loddo (2013)
+# Licence GPL
 
 # Read the kernel command line variable 'hostname'; this crude hack is
-# needed because filesystems are mounted read-only at this stage
-export `tr ' ' '\n' < /proc/cmdline 2> /dev/null | grep hostname=` &> /dev/null
+# needed because filesystems are mounted read-only at this stage.
+# The following command became something like:
+# export hostname=m1
+export $(tr ' ' '\n' </proc/cmdline | grep "^hostname=") &>/dev/null
 
 # Show a first message in the terminal window title bar...
 echo -en '\033]0;'Booting up the virtual machine \"$hostname\"...'\a'
+
